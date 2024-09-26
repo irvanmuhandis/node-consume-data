803557bb657a42a3f1b9f71fbcc0582f4456a834
Irvan Dev <irvanmuhandis@gmail.com>
2024-09-26 16:52:59 +0700

    Change server host route

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ const userService = `http://localhost:${USER_SERVICE_PORT}`;
 const init = async () => {
   const server = Hapi.server({
     port: 3000,
-    host: "localhost",
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+        cors: {
+            origin: ['*'],
+        },
+    },
   });
   server.route([
     {
